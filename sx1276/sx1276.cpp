2e2dd7ed1ea3ac4f76def61b20ea6b755de56dc6
Helmut Tschemernjak <Helmut Tschemernjak@localhost>
2018-06-22 11:07:18 +0200

    Changed var name to match last change

--- sx1276/sx1276.cpp
+++ sx1276/sx1276.cpp
@@ -1058,7 +1058,7 @@ void SX1276::StartCad( void )
 
 void SX1276::SetTxContinuousWave( uint32_t freq, int8_t power, uint16_t time_secs )
 {
-    uint32_t timeout = ( uint32_t )( time * 1e6 );
+    uint32_t timeout = ( uint32_t )( time_secs * 1e6 );
     
     SetChannel( freq );
     
